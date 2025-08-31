5dfeb8a5a03ea869acdf825c2018b8875849db91
georg5ru <151039020+georg5ru@users.noreply.github.com>
2025-08-31 23:21:56 +0300

    Create db_manager.py

---
+++ db_manager.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+
+class DBManager:
+    def __init__(self, dbname, user, password, host="localhost"):
+        self.conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
+        self.cursor = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self):
+        self.cursor.execute("""
+            SELECT companies.name, COUNT(vacancies.id) AS vacancies_count
+            FROM companies
+            LEFT JOIN vacancies ON companies.id = vacancies.company_id
+            GROUP BY companies.name
+        """)
+        return self.cursor.fetchall()
+
+    def get_all_vacancies(self):
+        self.cursor.execute("""
+            SELECT companies.name, vacancies.title, vacancies.url
+            FROM vacancies
+            JOIN companies ON vacancies.company_id = companies.id;
+        """)
+        return self.cursor.fetchall()
+
+    def get_average_salary(self):
+        self.cursor.execute("SELECT AVG(salary) FROM vacancies;")
+        return self.cursor.fetchone()[0]
+
+    def get_vacancies_with_higher_salary(self):
+        """Retrieves vacancies with salaries above the average."""
+        avg_salary = self.get_average_salary()
+        self.cursor.execute("""
+            SELECT title, salary, url
+            FROM vacancies
+            WHERE salary > %s;
+        """, (avg_salary,))
+        return self.cursor.fetchall()
+
+    def get_vacancies_with_keyword(self, keyword):
+
+        self.cursor.execute("""
+            SELECT title, salary, url
+            FROM vacancies
+            WHERE title LIKE %s;
+        """, (f'%{keyword}%',))
+        return self.cursor.fetchall()
